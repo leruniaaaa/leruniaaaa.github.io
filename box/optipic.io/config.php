361a79b61072637ccd3174c7e443db41725ffd76
leruniaaaa <71155438+leruniaaaa@users.noreply.github.com>
2020-09-30 14:37:23 +0300

    Add files via upload

---
+++ box/optipic.io/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Config of OptiPic.io plugin
+ * 
+ * @author https://optipic.io/
+ * @copyright (c) 2018, https://optipic.io
+ */
+
+$optipicCfg = array(
+    "secretkey" => "WNXWL5bSRVaQ2TMv70DwVDkZrYSov89y6nD7t5su2_9Vqt_2DeSr8i1MEa7ViMxMxW2RtOPcfRgyglfjMUuPWwIY-_aNf3YZWHmEGLLqjO7_o1V2jEXVOGlcGcY9SBfctuErFigmxQZh1lMP2qlcK6gTOvVItZZrkk7ad7kDfEKzlYR9B1-M_ZU8jxLop6yaXdyVFkRBJDv6-pKepnHKkmDQL2fIHGdp_gDplFkaoif7cKtpDKbE0Ls37bNDlZt",
+    "api_url" => "https://optipic.io/api/",
+);
+?>
