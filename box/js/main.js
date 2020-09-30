78fcc9e8b74d9af4bf60668f9712dcaa4eb65810
leruniaaaa <71155438+leruniaaaa@users.noreply.github.com>
2020-09-30 14:00:23 +0300

    замена js
    замена js

--- box/js/main.js
+++ box/js/main.js
@@ -5,6 +5,17 @@ $(function() {
 	let scrollPos = $(window).scrollTop();
 	let nav = $("#nav");
 	let navToggle = $("#navToggle");
+	let how = $("#how");
+	let howTop = how.offset().top;
+
+	$(window).bind('scroll', function() {
+		let windowTop = $(this).scrollTop();
+		if (windowTop > howTop) {
+			$('.contact__map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A690b11f8ab72eba93d13c1fb4e2766344a8002a381690ae291d41785755b60f4&amp;width=400&amp;height=300&amp;lang=ru_RU&amp;scroll=true"></script>')
+			$(window).unbind('scroll');
+		}
+	});
+
 
 	checkScroll(scrollPos, introH);
 
